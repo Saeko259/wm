d3a10ddd72a641b450a9c169352f3b40cd58784f
Saeko259 <Samueltellodiaz@gmail.com>
2023-06-06 19:58:29 -0500

    cambios

--- mezcla/modal/index.js
+++ mezcla/modal/index.js
@@ -1,6 +1,9 @@
 const openModal = document.querySelector('.click')
+const openModal2 = document.querySelector('.click2')
 const modal = document.querySelector('.modal')
+const modal2 = document.querySelector('.modal2')
 const closeModal = document.querySelector('.modal_close')
+const closeModal2 = document.querySelector('.modal_close2')
 
 openModal.addEventListener('click', (e)=>{
     e.preventDefault()
@@ -12,3 +15,15 @@ closeModal.addEventListener('click', (e)=>{
     e.preventDefault()
     modal.classList.remove('modal--show')
 })
+
+
+openModal2.addEventListener('click', (e)=>{
+    e.preventDefault()
+    modal2.classList.add('modal2--show')
+})
+
+closeModal2.addEventListener('click', (e)=>{
+    e.preventDefault()
+    modal2.classList.remove('modal2--show')
+})
+
