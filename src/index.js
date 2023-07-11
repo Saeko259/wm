efbffd885a37fde549f6f1551fb28ef661047977
Saeko259 <Samueltellodiaz@gmail.com>
2023-07-11 18:36:54 -0500

    new app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { toBePartiallyChecked } from "@testing-library/jest-dom/matchers";
+import "./style.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const prueba = ReactDOM.createRoot(document.getElementById('prueba'))
+
+function Saludo() {
+    return <div className="main" >
+        <div className="square">
+            
+            <br></br>
+            <div className="topsquare">
+                <p>
+                    HELLO!</p>
+            </div>
+            <div className="bottomsquare">
+                <b>Mi name is Samuel</b>   
+                <p>Quiero aprender a programar con react</p>
+            </div>
+        </div>
+    </div>
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div>
+        {Saludo()}
+    </div>
+)
